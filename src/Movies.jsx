58896453ed443965c3fc96050f6623a93a0a3180
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-05 18:51:11 -0300

    Fix Errors

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -3,12 +3,12 @@ import Image from "./Image"
 function Movies(urlMovie){
   
   return(
-    Object.entries(urlMovie).map((name, movie) =>{
+    urlMovie.map((name, movie) =>{
       <>
       <h1>My favourites movies!</h1>
       <Image key={key={name}} movies={movie}/>
-      <Image key={index} movies={movie}/>
-      <Image key={index} movies={movie}/>
+      <Image key={key={name}} movies={movie}/>
+      <Image key={key={name}} movies={movie}/>
     
     </>
     }) 
