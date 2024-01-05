6578506291d9eb76cf27d68381ac052a17f0b0e1
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-05 18:59:19 -0300

    Last commit

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -2,17 +2,17 @@ import Image from "./Image"
 
 function Movies(urlMovie){
   
-  return(
-    urlMovie.map((name, movie) =>{
+  
+    const moviesIMages = Object.entries(urlMovie).map((name, movie) =>{
       <>
-      <h1>My favourites movies!</h1>
-      <Image key={key={name}} movies={movie}/>
-      <Image key={key={name}} movies={movie}/>
-      <Image key={key={name}} movies={movie}/>
-    
-    </>
-    }) 
-  ) 
+        <h1>My favourites movies!</h1>
+        <Image key={key={name}} movies={movie}/>
+        <Image key={key={name}} movies={movie}/>
+        <Image key={key={name}} movies={movie}/>
+      </>
+    }
+  )
+  return moviesIMages
 }
 
 export default Movies
