b5b6754b62242225e942ac4557f83c1f642e682b
Big3ppv <joaquinruizperez2002@gmail.com>
2024-01-16 18:05:36 -0300

    Change name

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MovieList from "./Components/MovieList"
+
+function App() {
+  return(
+    <div>
+      <MovieList/>
+    </div>
+  )
+}
+export default App
---
+++ src/Components/MovieList/NameTittle/index.jsx
@@ -0,0 +1,15 @@
+const NameTittle = (props) =>{
+    const titleName = props.nameAndDescription
+    return(
+        <div>
+            {titleName.map((values) =>{
+                <div key={values.id} className="bg-red-500">
+                <h2>{values.title}</h2>
+                <p>{values.description}</p>
+                </div>
+            })}
+        </div>
+    )
+}
+
+export default NameTittle
